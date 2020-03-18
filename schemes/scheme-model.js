74f000318cd6b2c43d8d0a1814cc7a20eefa1bb1
Josue Rodriguez <smexyjosue@gmail.com>
2020-03-18 12:57:08 -0700

    Create skeleton structure for sql functions in sheme-model.js
    * Added base/skeleton fuctions in sheme-model.js for methods used in the sheme-router.js

---
+++ schemes/scheme-model.js
@@ -0,0 +1,23 @@
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  addStep,
+  update,
+  remove
+};
+
+function find() {}
+
+function findById() {}
+
+function findSteps() {}
+
+function add() {}
+
+function addStep() {}
+
+function update() {}
+
+function remove() {}
