29e687677a82ad81e14876e0ca07c36badda8bef
Josue Rodriguez <smexyjosue@gmail.com>
2020-03-18 19:36:23 -0700

    Complete queries on sql studio

--- queries.sql
+++ queries.sql
@@ -2,8 +2,34 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+-- SELECT Product.ProductName, Category.CategoryName
+-- FROM [Product]
+-- JOIN [Category]
+-- ON Product.CategoryId = Category.Id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+-- SELECT o.Id, Shipper.CompanyName, o.OrderDate
+-- FROM [Order] as o
+-- JOIN [Shipper]
+-- ON o.ShipVia = Shipper.id
+-- WHERE o.OrderDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+-- SELECT OrderDetail.OrderId, p.ProductName, OrderDetail.Quantity
+-- FROM [OrderDetail]
+-- JOIN [Product] as p
+-- ON OrderDetail.ProductId = p.Id
+-- WHERE OrderDetail.OrderId = '10251'
+-- ORDER BY ProductName;
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+-- SELECT o.Id as OrderNum, c.CompanyName as Company_Name, e.LastName as Employee_last_Name
+-- FROM [Order] as o
+-- JOIN [Customer] as c
+-- ON o.CustomerId = c.Id
+-- JOIN [Employee] as e
+-- ON o.EmployeeId = e.Id
+-- ORDER BY Company_Name;
